c1763918c39bc1fc8bc8881590d40c68efbbd7c3
Vijay Kumar <ind.vijayworldz@gmail.com>
2016-03-14 23:15:37 +0530

    Initial gulp ,browserify and package setup for the application

---
+++ contactlist-withflux/gulp.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp'),
+    browserify = require('browserify'),
+    reactify = require('reactify'),
+    source = require('source');
+
+//automating gulp task for browserify
+gulp.task('browserify',function(){
+    browserify('./src/js/index.js')
+    .transform('reactify')
+    .bundle()
+    .pipe(source('index.js'))
+    .pipe(gulp.dest('dist/js'));
+});
+
+//automating copy to dist location
+gulp.task('copy',function(){
+    gulp.src('src/index.html')
+    .pipe(gulp.dest('dist'));
+    gulp.src('src/css/*.*')
+        .pipe(gulp.dest('dist/css'));
+    gulp.src('src/js/vendors/*.*')
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('default',['browserify','copy'],function(){
+    return gulp.watch('src/**/*.*',['browserify','copy']);
+});
